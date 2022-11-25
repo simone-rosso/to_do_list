d1761d49847788d560a3155593356f5fa6eb0c66
Simone <simon.red.au@gmail.com>
2022-11-25 18:13:05 +0100

    fixed error with root

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import * as ReactDOMClient from 'react-dom/client';
 import App from './App'
-import { initializeApp } from "firebase/app";
 
-const app = initializeApp({
-    apiKey: "AIzaSyDwET9DroVo0qggKtCr7g714khgyaet6Z4",
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-});
 
 import './styles.css'
 
